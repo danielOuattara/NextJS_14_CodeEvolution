44accec7321f6c5efce2a781007e9e7b2c6b892f
danielOuattara <daniel.ouattara@gmx.com>
2023-11-17 11:59:43 +0100

    16 route group layout

---
+++ _16_route_group_layout/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+function BarChart() {
+  return <h1>Bar chart</h1>;
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      {/* <BarChart /> */}
+    </>
+  );
+}
