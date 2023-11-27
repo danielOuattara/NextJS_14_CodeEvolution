ad764509c93249e87a0fabdf6331db0b091c1476
danielOuattara <daniel.ouattara@gmx.com>
2023-11-27 23:15:07 +0100

    22 templates

---
+++ _22_templates/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1> Welcome ! Home page</h1>
+      <Link href={"/blog"}>Visit Blogs</Link>
+      <br />
+      <Link href={"/products"}>Visit Products</Link>
+    </>
+  );
+}
