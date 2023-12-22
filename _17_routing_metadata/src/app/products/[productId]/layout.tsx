c663a013521535cfaaf06a5d79349b0f03a3580c
danielOuattara <daniel.ouattara@gmx.com>
2023-12-22 20:13:09 +0100

    17 added generateMetadata() in layout/tsx

--- _17_routing_metadata/src/app/products/[productId]/layout.tsx
+++ _17_routing_metadata/src/app/products/[productId]/layout.tsx
@@ -1,18 +1,64 @@
+/* 
+NOTE: generateMetadata() can run sync OR async
+
+*/
+
+//---------------------------------------------------------------
+import type { Metadata, ResolvingMetadata } from "next";
+type Props = {
+  params: { productId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  /* read route params */
+  // const id = params.id;
+
+  /* fetch data */
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json());
+
+  /* optionally access and extend (rather than replace) parent metadata */
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  const productTitle: string = await new Promise((resolve) =>
+    setTimeout(() => resolve(`iPhone ${params.productId}`), 3000),
+  );
+
+  return {
+    // title: `product ${params.productId}`, // OR
+    title: productTitle,
+    // openGraph: {
+    // images: ["/some-specific-page-image.jpg", ...previousImages],
+    // },
+    description: `description for product ${params.productId}`,
+  };
+}
+
+//---------------------------------------------------------------
+
+type LayoutPropsType = {
+  children: React.ReactNode;
+  params: string;
+};
+
 export default function SingleProductLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  params,
+}: LayoutPropsType) {
   return (
     <article style={singleProduct}>
       <h2>Single product page</h2>
       {children}
+
       {/* Carousel here */}
     </article>
   );
 }
 
-const singleProduct = {
+const singleProduct: React.CSSProperties = {
   backgroundColor: "orange",
   padding: "10px",
   textAlign: "center",
