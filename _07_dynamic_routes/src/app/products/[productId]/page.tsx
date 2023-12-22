623a2d805b00fc66f388cdc6c98bacd4fffc436d
danielOuattara <daniel.ouattara@gmx.com>
2023-12-22 20:09:11 +0100

    07 update Type definition

--- _07_dynamic_routes/src/app/products/[productId]/page.tsx
+++ _07_dynamic_routes/src/app/products/[productId]/page.tsx
@@ -1,9 +1,22 @@
-type Params = {
+// type PropsType = {
+//   params: {
+//     productId: string;
+//   };
+//   searchParams: string;
+// };
+
+// export default function SingleProduct(props: PropsType) {
+//   return <p>Details page about product {props.params.productId}</p>;
+// }
+
+//---------------------------------- OR --
+
+type ParamsType = {
   params: {
     productId: string;
   };
 };
 
-export default function SingleProduct({ params: { productId } }: Params) {
+export default function SingleProduct({ params: { productId } }: ParamsType) {
   return <p>Details page about product {productId}</p>;
 }
