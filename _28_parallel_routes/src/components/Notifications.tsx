1aaa65dc8ade29ffa1187408ff64be4dc1a10986
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:35:30 +0100

    28  parallel routes

---
+++ _28_parallel_routes/src/components/Notifications.tsx
@@ -0,0 +1,5 @@
+import Card from "./Card";
+
+export default function Notifications() {
+  return <Card>Notifications</Card>;
+}
---
+++ _28_parallel_routes/src/components/RevenueMetrics.tsx
@@ -0,0 +1,5 @@
+import Card from "./Card";
+
+export default function RevenueMetrics() {
+  return <Card>RevenueMetrics</Card>;
+}
