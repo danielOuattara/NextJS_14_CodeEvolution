a574827ac4d9f0c44ccaf75f5ae135d070836d1a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:36:00 +0100

    30 conditional routes

---
+++ _30_conditional_routes/src/app/dashboard-parallel-routes/default.tsx
@@ -0,0 +1,7 @@
+export default function DefaultDashboardOldBundle() {
+  return (
+    <>
+      <h1>Dashboard Parallel Routes Page</h1>
+    </>
+  );
+}
---
+++ _30_conditional_routes/src/components/UserAnalytics.tsx
@@ -0,0 +1,3 @@
+export default function UserAlaytics() {
+  return <div>UserAlaytics</div>;
+}
