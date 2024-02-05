641e63ef7eeb9b5c370dbb863b2fda8e3d1e27fa
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:34:38 +0100

    27 handle errors in layout

---
+++ _27_handling_errors_in_layouts/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+type ErrorType = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function ErrorBoundary({ error, reset }: ErrorType) {
+  return (
+    <>
+      <div style={{ backgroundColor: "red" }}>
+        Error in reviewId: {error.message}
+      </div>
+      <button onClick={reset}>Try again !</button>
+    </>
+  );
+}
