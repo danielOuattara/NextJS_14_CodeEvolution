c31cb67f16f6c674bef50bdf2108a39228a955d4
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-12 18:24:50 +0100

    39 url query parameters

---
+++ _39_url_query_parameters/src/app/comments/[commentId]/route.ts
@@ -0,0 +1,57 @@
+import { comments } from "../data";
+
+type TypeContext = {
+    params: {
+        commentId: string;
+    };
+};
+
+//--------------------------------------------------------------------
+export async function GET(request: Request, context: TypeContext) {
+    const comment = comments.find(
+        (item) => item.id === parseInt(context.params.commentId),
+    );
+
+    if (!comment) {
+        return new Response("Comment Not Found", {
+            status: 404
+        })
+    }
+    return Response.json({ comment });
+}
+
+//--------------------------------------------------------------------
+export async function PATCH(request: Request, context: TypeContext) {
+    const commentIndex = comments.findIndex(
+        (item) => item.id === parseInt(context.params.commentId),
+    );
+    if (commentIndex === -1) {
+        return new Response("Comment Not Found", {
+            status: 404
+        })
+    }
+
+    const body = await request.json()
+
+    comments[commentIndex] = { ...comments[commentIndex], ...body }
+
+    return Response.json({ comments });
+}
+
+
+//--------------------------------------------------------------------
+export async function DELETE(request: Request, context: TypeContext) {
+    const commentIndex = comments.findIndex(
+        (item) => item.id === parseInt(context.params.commentId),
+    );
+    if (commentIndex === -1) {
+        return new Response("Comment Not Found", {
+            status: 404
+        })
+    }
+
+    comments.splice(commentIndex, 1)
+
+    return Response.json({ comments });
+}
+
