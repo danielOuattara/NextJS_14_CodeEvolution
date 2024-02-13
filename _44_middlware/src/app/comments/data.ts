0ae8d94ae34a92c09e2294d32e9fb14212ba3128
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-13 16:13:44 +0100

    44 middlewares

---
+++ _44_middlware/src/app/comments/data.ts
@@ -0,0 +1,5 @@
+export const comments = [
+    { id: 1, text: "comment first n°01" },
+    { id: 2, text: "comment second n°02" },
+    { id: 3, text: "comment third n°03" },
+]
---
+++ _44_middlware/src/middleware.ts
@@ -0,0 +1,45 @@
+// import { type NextRequest, NextResponse } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/", request.url));
+// }
+
+// export const config = {
+//   matcher: ["/profile"],
+// };
+
+//--------------------------------------------------------------------
+
+// import { type NextRequest, NextResponse } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname === "/profile") {
+//     return NextResponse.redirect(new URL("/hello", request.url));
+//   }
+// }
+
+//--------------------------------------------------------------------
+
+// import { type NextRequest, NextResponse } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname === "/profile") {
+//     return NextResponse.rewrite(new URL("/hello", request.url));
+//   }
+// }
+
+//--------------------------------------------------------------------
+
+import { type NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
+  }
+
+  response.headers.set("custom-header", "custom-value");
+
+  return response;
+}
