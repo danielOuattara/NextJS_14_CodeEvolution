45f0b6c8c52f28c5a053c9c046d944913f16ef1e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-04 12:23:52 +0100

    50 Server & Client Components

---
+++ _50_Server_Components_and_Client_Components/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function DashboardPage() {
+  console.log("dash board page");
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <button onClick={() => console.log("Clicked!")}>click</button>
+    </>
+  );
+}
