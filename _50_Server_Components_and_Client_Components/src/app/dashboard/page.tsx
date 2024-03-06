a47eb861d7075135e952cf198c1992e5d5e127ee
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-06 12:11:16 +0100

    50 server & client components

--- _50_Server_Components_and_Client_Components/src/app/dashboard/page.tsx
+++ _50_Server_Components_and_Client_Components/src/app/dashboard/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
+import { useState } from "react";
+
 export default function DashboardPage() {
   console.log("dash board page");
+  const [name, setName] = useState("");
   return (
     <>
       <h1>Dashboard Page</h1>
-      <button onClick={() => console.log("Clicked!")}>click</button>
+      <button
+        onClick={() => console.log("Clicked!")}
+        style={{
+          background: "lightblue",
+          padding: "5px 30px",
+          margin: "10px",
+          borderRadius: "5px",
+        }}
+      >
+        click
+      </button>
+      <br />
+      <label htmlFor="name">
+        Name:
+        <input
+          style={{ border: "1px solid lightblue", marginLeft: "10px" }}
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      {name && <h2>Hello {name}</h2>}
     </>
   );
 }
