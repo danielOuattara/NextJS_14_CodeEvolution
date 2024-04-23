2868292f2b0a3e3228b209008ada37f57908a372
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-23 19:31:38 +0200

    12 private folder : updated

--- _12_private_folder/src/app/products/[productId]/page.tsx
+++
@@ -1,9 +0,0 @@
-type Params = {
-  params: {
-    productId: string;
-  };
-};
-
-export default function SingleProduct({ params: { productId } }: Params) {
-  return <h1>Details page about product {productId}</h1>;
-}
