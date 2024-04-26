06e8bf7f81204592d89aa9c4fdb2d4f26c4c13a4
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-26 23:38:27 +0200

    39 url query parameters: updated

--- _39_url_query_parameters/src/app/comments/route.ts
+++ _39_url_query_parameters/src/app/comments/route.ts
@@ -16,36 +16,46 @@ import { comments } from "./data";
 //   return Response.json({ hits: comments.length, comments });
 // }
 
-//=====
+//--------------------------------------------------------------------
 
+// export async function GET(request: NextRequest) {
+//   const searchParams = request.nextUrl.searchParams;
+//   const query = searchParams.get("query");
+//   const filteredComments = query
+//     ? comments.filter((comment) => comment.text.includes(query))
+//     : comments;
+//   return Response.json(filteredComments);
+// }
+
+//--------------------------------------------------------------------
+
+/*--------- one way of getting params -------------*/
+// export async function GET(request: NextRequest) {
+//   // const { searchParams } = new URL(request.url);
+//   // OR
+//   const { searchParams } = new URL(request.nextUrl);
+//   const query = searchParams.get("query");
+//   const filteredComments = query
+//     ? comments.filter((comment) => comment.text.includes(query))
+//     : comments;
+//   return Response.json(filteredComments);
+// }
+
+//--------------------------------------------------------------------
+
+/*--------  another efficient way of filtering on all queries params ------*/
 export async function GET(request: NextRequest) {
-  // const searchParams = request.nextUrl.searchParams;
-  // const query = searchParams.get("query");
-  // const filteredComments = query
-  //   ? comments.filter((comment) => comment.text.includes(query))
-  //   : comments;
-  // return Response.json({ filteredComments });
-
-  // /*--------- one way of getting params -------------*/
-
-  // const { searchParams } = new URL(request.url);
-  // const query = searchParams.get("query");
-  // const filteredComments = query
-  //   ? comments.filter((comment) => comment.text.includes(query))
-  //   : comments;
-  // return Response.json({ filteredComments });
-
-  // /*--------  another efficient way of getting params ------*/
-
-  const { searchParams } = new URL(request.url);
+  const { searchParams } = new URL(request.nextUrl);
   const obj = Object.fromEntries(searchParams.entries());
-  let filteredComments = undefined;
+
+  let filteredComments: { [k: string]: string | number }[] = [];
+
   for (const key in obj) {
     filteredComments = key
       ? comments.filter((comment) => comment.text.includes(obj[key]))
       : comments;
   }
-  return Response.json({ filteredComments });
+  return Response.json(filteredComments);
 }
 
 //--------------------------------------------------------------------
