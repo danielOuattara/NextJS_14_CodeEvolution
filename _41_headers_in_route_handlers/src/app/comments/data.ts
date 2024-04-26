481b3f56f28e37a708b3b2a8c333b3f8c289d0a1
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-26 23:39:12 +0200

    41 headers in route handlers: updated

--- _41_headers_in_route_handlers/src/app/comments/data.ts
+++
@@ -1,5 +0,0 @@
-export const comments = [
-    { id: 1, text: "comment first n°01" },
-    { id: 2, text: "comment second n°02" },
-    { id: 3, text: "comment third n°03" },
-]
--- _41_headers_in_route_handlers/src/app/profile/api/route.ts
+++ _41_headers_in_route_handlers/src/app/profile/api/route.ts
@@ -2,21 +2,24 @@ import { type NextRequest } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
-  //------------------------------------------------
+  //------
 
   console.log(request.headers.get("Authorization"));
+  console.log(request.headers.get("secret"));
 
-  //------------------------------------------------ OR
+  //------ OR
 
   const requestHeaders = new Headers(request.headers);
   console.log(requestHeaders.get("Authorization"));
+  console.log(requestHeaders.get("secret"));
 
-  //------------------------------------------------ OR
+  //------ OR
 
   const headerList = headers();
   console.log(headerList.get("Authorization"));
+  console.log(headerList.get("secret"));
 
-  //------------------------------------------------- OR
+  //------- OR
 
   // return new Response("Profile API data !");
   return new Response("<h1>Profile API data !</h1>", {
