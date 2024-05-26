5f8b3c67637c9c808e46303c686d0c9bdaaa995e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-27 01:03:16 +0200

    63 fetching with server components

---
+++ _63_fetching_with_server_components/src/app/users/page.tsx
@@ -0,0 +1,31 @@
+type TypeUser = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default async function UserPage() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: TypeUser[] = await res.json();
+  return (
+    <div className="grid sm:grid-cols-2 gap-2 p-4 max-w-screen-md m-auto">
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-gray-600"
+        >
+          <div className="flex flex-col space-y-1">
+            <h2 className="text-lg font-semibold">{user.name}</h2>
+            <p className="text-sm">{user.username}</p>
+          </div>
+          <div className="flex flex-col space-y-1 items-end">
+            <div className="text-md">{user.email}</div>
+            <div className="text-md">{user.phone}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
